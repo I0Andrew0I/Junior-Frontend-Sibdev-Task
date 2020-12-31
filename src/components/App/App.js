f9ca6ba99135ca4873e072c72f4e818a50b6dc45
I0Andrew0I <43104794+I0Andrew0I@users.noreply.github.com>
2020-12-31 15:08:43 +0700

    Import fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import AuthPage from "../../pages/AuthPage/AuthPage";
 import IndexPage from "../../pages/IndexPage/IndexPage";
-import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 
 const App = () => {
   const isAuthorized = Boolean(localStorage.getItem("token"));
