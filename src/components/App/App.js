c2c9df09efc30035013839f703851bdb16dfdb83
I0Andrew0I <43104794+I0Andrew0I@users.noreply.github.com>
2020-12-31 15:07:56 +0700

    Redirect fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import AuthPage from "../../pages/AuthPage/AuthPage";
 import IndexPage from "../../pages/IndexPage/IndexPage";
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
@@ -11,7 +11,7 @@ const App = () => {
       <Switch>
         <Route path="/auth" component={AuthPage} />
         {isAuthorized && <Route path="*" component={IndexPage} />}
-        {!isAuthorized && <Route path="*" component={NotFoundPage} />}
+        <Redirect to="/auth" />
       </Switch>
     </div>
   );
